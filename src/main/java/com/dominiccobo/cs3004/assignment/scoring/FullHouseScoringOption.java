c4b4e4774d587170dae4c9d41e2c5e203c5c92d4
Dominic Cobo <dominiccobo@gmail.com>
2019-10-21 11:47:34 +0100

    Simplify Full house verification logic

--- src/main/java/com/dominiccobo/cs3004/assignment/scoring/FullHouseScoringOption.java
+++ src/main/java/com/dominiccobo/cs3004/assignment/scoring/FullHouseScoringOption.java
@@ -2,6 +2,8 @@ package com.dominiccobo.cs3004.assignment.scoring;
 
 import com.dominiccobo.cs3004.assignment.ScoringOption;
 
+import java.util.*;
+
 /**
  * TODO: add class description.
  *
@@ -18,56 +20,8 @@ public class FullHouseScoringOption extends ScoringOption {
     @Override
     public void checkAgainstDice(int[] theDice) {
         if (!this.hasScoredAlready()) {
-            //CHECK FULL HOUSE
-            int count = 0;
-            //Check it's not a Y
-            for (int i = 0; i < 4; i++) {
-                if (theDice[0] != theDice[i]) {
-                    count++;
-                }
-            }
-            if (count > 0) {
-                //Two conditions 2 then 3 or 3 then 2
-
-                count = 0;
-
-                //Check first two the same then check last three
-                for (int i = 0; i < 2; i++) {
-                    if (theDice[0] != theDice[i]) {
-                        count++;
-                    }
-                }
-
-                //Check last three the same
-                for (int i = 2; i < 5; i++) {
-                    if (theDice[2] != theDice[i]) {
-                        count++;
-                    }
-                }
-
-                if (count == 0) {
-                    this.canBeAwarded = true;
-                } else {
-                    count = 0;
-                    //Check first three the same then check last two
-                    for (int i = 0; i < 3; i++) {
-                        if (theDice[0] != theDice[i]) {
-                            count++;
-                        }
-                    }
-
-                    //Check last three the same
-                    for (int i = 3; i < 5; i++) {
-                        if (theDice[3] != theDice[i]) {
-                            count++;
-                        }
-                    }
-
-                    if (count == 0) {
-                        canBeAwarded = true;
-                    }
-
-                }
+            if (hasTwoOfOneAndThreeOfAnother(theDice)) {
+                this.canBeAwarded = true;
             }
         }
     }
@@ -75,5 +29,27 @@ public class FullHouseScoringOption extends ScoringOption {
     @Override
     public String getScoringOptionName() {
         return SCORING_OPTION_NAME;
+    }
+
+    private Map<Integer, Integer> countInstancesOfNumberInRoll(int[] rolledDice) {
+        Map<Integer, Integer> instancesOfValue = new HashMap<>();
+        for (int number : rolledDice) {
+            Integer val = instancesOfValue.getOrDefault(number, 0);
+            Integer newValue = ++val;
+            instancesOfValue.put(number, newValue);
+        }
+        return instancesOfValue;
+    }
+
+    private boolean hasTwoOfOneAndThreeOfAnother(int[] theDice) {
+        final Map<Integer, Integer> instancesOfValue = countInstancesOfNumberInRoll(theDice);
+        final Set<Integer> numberOfInstances = new HashSet<>();
+
+        for (Integer integer : instancesOfValue.keySet()) {
+            numberOfInstances.add(instancesOfValue.get(integer));
+        }
+        final List<Integer> mustContainTheseValues = Arrays.asList(3, 2);
+
+        return (numberOfInstances.containsAll(mustContainTheseValues));
     }
 }
